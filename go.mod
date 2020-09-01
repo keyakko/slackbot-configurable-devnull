ec48567fbcce9ac1696f90c5bebcf46cd385cbc1
keyakko <keyakko.web@gmail.com>
2020-09-01 23:45:55 +0900

    [WIP] update

--- go.mod
+++ go.mod
@@ -1,3 +1,9 @@
 module github.com/keyakko/slackbot-configurable-devnull
 
 go 1.14
+
+require (
+	github.com/BurntSushi/toml v0.3.1 // indirect
+	github.com/robfig/cron/v3 v3.0.1 // indirect
+	github.com/slack-go/slack v0.6.6 // indirect
+)
--- main.go
+++ main.go
@@ -2,29 +2,58 @@ package main
 
 import (
 	"fmt"
+	"strconv"
 	"time"
+
 	"github.com/BurntSushi/toml"
 	"github.com/robfig/cron/v3"
-  "github.com/nlopes/slack"
+	"github.com/slack-go/slack"
 )
 
 type tomlConfig struct {
-	SlackToken string `toml:"slack_token"`
+	SlackToken     string                   `toml:"slack_token"`
 	TargetChannels map[string]targetChannel `toml:"target_channels"`
 }
 
 type targetChannel struct {
-	ChannelId string `toml:"channel_id"`
-	Timer string `toml:"timer"`
+	ChannelID string `toml:"channel_id"`
+	Timer     int64  `toml:"timer"`
 }
 
+func cleanChats(slackClient *slack.Client, config tomlConfig) {
+	// apiCallCounter := 0
+	currentTime := time.Now().Unix()
 
+	for _, targetChannelConf := range config.TargetChannels {
+		fmt.Println(targetChannelConf.ChannelID)
+		fmt.Println(targetChannelConf.Timer)
 
-func cleanChats(slackApi *slack.Client, config *tomlConfig) {
-	apiCallCounter := 0
+		var getConversasionHistoryParameters slack.GetConversationHistoryParameters
+		getConversasionHistoryParameters.ChannelID = targetChannelConf.ChannelID
+		getConversasionHistoryParameters.Latest = strconv.FormatInt(currentTime-targetChannelConf.Timer, 10)
+		getConversasionHistoryParameters.Cursor = ""
+		getConversasionHistoryParameters.Inclusive = false
+		getConversasionHistoryParameters.Limit = 100
+		getConversasionHistoryParameters.Oldest = "0"
+
+		conversationHistory, err := slackClient.GetConversationHistory(
+			&getConversasionHistoryParameters,
+		)
+		// apiCallCounter++
+		if err != nil {
+			fmt.Println("[ERROR] getConversationHistory failed.")
+			fmt.Println(err)
+			continue
+		}
+
+		for _, message := range conversationHistory.Messages {
+			slackClient.DeleteMessage(targetChannelConf.ChannelID, message.Timestamp)
+		}
+
+	}
 }
 
-func main () {
+func main() {
 
 	// load config
 	var config tomlConfig
@@ -33,10 +62,12 @@ func main () {
 		return
 	}
 
-	slackApi := slack.New(config.SlackToken)
+	slackClient := slack.New(config.SlackToken)
 	cronInstance := cron.New()
 
-	cronInstance.AddFunc("@every 1m", cleanChats(slackApi, config))
+	cronInstance.AddFunc("@every 1m", func() { cleanChats(slackClient, config) })
+	cronInstance.Start()
+	cleanChats(slackClient, config)
 
 	for {
 		time.Sleep(86400 * time.Second)
